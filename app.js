a3c943076163e615f84337702c133683f07e751f
neerajp99 <neerajp1999@gmail.com>
2019-11-19 18:21:21 +0530

    Add body-parser middleware and establish database connection

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const keys = require("./config/keys");
 
+// iniaitize app
 const app = express();
+
+// body-parser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// establish db connection
+const db = keys.mongoURI;
+mongoose
+  .connect(
+    db,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(res => {
+    console.log("Database connected successully");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.json("lalalal");
+});
+
+// add listening port
+const port = process.env.PORT || 5006;
+app.listen(port, () => {
+  console.log(`App running at port ${port}`);
+});
 
 module.exports = app;
