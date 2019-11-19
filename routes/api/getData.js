ea48f28eaa35ee1cf6a8d2a48c8ed6c6f387c11c
neerajp99 <neerajp1999@gmail.com>
2019-11-19 19:26:02 +0530

    Add initial route for get endpoint

---
+++ routes/api/getData.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const quotes = require('../../utils/quotes.json')
+const json = require('json')
+
+
+router.get('/', (req, res ) => {
+
+})
+
+
+// export router
+module.exports = router
