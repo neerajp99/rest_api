df903d6605da6a6ab57112a35e47e51f97f698e6
neerajp99 <neerajp1999@gmail.com>
2019-11-20 17:13:54 +0530

    Modify the get method for the api

--- routes/api/getData.js
+++ routes/api/getData.js
@@ -1,13 +1,33 @@
-const express = require('express')
-const router = express.Router()
-const quotes = require('../../utils/quotes.json')
-const json = require('json')
+const express = require("express");
+const router = express.Router();
+const quotes = require("../../utils/quotes.json");
+const json = require("json");
 
+router.get("/:id", (req, res, next) => {
+  // const quotesData = JSON.parse(quotes)
+  try {
+    const quoteFound = [];
+    quotes.map(data => {
+      // console.log(data.author)
+      if (req.params.id === data.author) {
+        quoteFound.push(data);
+      }
+    });
 
-router.get('/', (req, res ) => {
-
-})
-
+    // const quoteFound = quotes.find(quote => quotes.author === "Ayn")
+    if (quoteFound.length == 0) {
+      const error = new Error("No quote found!");
+      error.status = 404;
+      throw error;
+      res.json(error);
+      quoteFound = [];
+    }
+    res.json(quoteFound);
+    quoteFound = [];
+  } catch (error) {
+    next(error);
+  }
+});
 
 // export router
-module.exports = router
+module.exports = router;
