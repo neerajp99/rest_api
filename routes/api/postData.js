923195bff480a088ab85450b660eccb010329b50
neerajp99 <neerajp1999@gmail.com>
2019-11-20 17:49:40 +0530

    Add post endpoint route

---
+++ routes/api/postData.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const quotes = require("../../utils/quotes.json");
+const fs = require('fs');
+
+router.post("/", (req, res, next) => {
+  try {
+    const newQuote = {
+      quote: req.body.author,
+      author: req.body.author,
+      category: req.body.category
+    };
+    quotes.push(newQuote);
+    fs.writeFileSync("utils/quotes.json", JSON.stringify(quotes));
+    res.status(201).json(quotes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
